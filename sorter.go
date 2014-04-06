09222c21118d69b6f5e4dd054736233f769d240d
Robin Thrift <robin@we-two-design.com>
2014-04-06 21:36:35 +0200

    fixed welcome text

--- sorter.go
+++ sorter.go
@@ -1,134 +1,135 @@
 package main
 
-
 import (
-    "fmt"
-    "os"
-    "flag"
-    "io/ioutil"
-    "io"
-    "strconv"
-    "time"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"time"
 )
-
 
 func main() {
 
-    cwd, _ := os.Getwd()
+	cwd, _ := os.Getwd()
 
+	var srcDir = flag.String("src", cwd, "Set the source directory from which to pull the files from.")
+	var outDirP = flag.String("out", "sorted", "Set the directory where the files will be coppied to.")
 
-    fmt.Println("Simple image sorter started:")
+	flag.Parse()
 
-    var srcDir = flag.String("src", cwd, "Set the source directory from which to pull the files from.")
-    var outDirP = flag.String("out", "sorted", "Set the directory where the files will be coppied to.")
+	outDir := *outDirP
 
-    flag.Parse()
+	fmt.Println("Copying and sorting files from " + *srcDir + " to " + outDir)
 
-    outDir := *outDirP
+	var imgList, err = ioutil.ReadDir(*srcDir)
 
-    var imgList, err = ioutil.ReadDir(*srcDir);
+	if err != nil {
+		return
+	}
 
-    if err != nil {
-        return
-    }
+	numFiles := len(imgList)
 
-    numFiles := len(imgList)
+	fileMap := make(map[int]map[string]map[string][]os.FileInfo)
 
-    fileMap := make(map[int]map[string]map[string][]os.FileInfo)
+	for _, _f := range imgList {
+		t := _f.ModTime()
+		y := t.Year()
+		m := t.Month().String()
+		d := strconv.Itoa(t.Day()) + " " + t.Weekday().String()
 
-    for _, _f := range imgList {
-        t := _f.ModTime()
-        y := t.Year()
-        m := t.Month().String()
-        d := strconv.Itoa(t.Day()) +  " " + t.Weekday().String()
+		if _, ok := fileMap[y]; !ok {
+			fileMap[y] = make(map[string]map[string][]os.FileInfo)
+		}
 
-        if _, ok := fileMap[y]; !ok {
-            fileMap[y] = make(map[string]map[string][]os.FileInfo)
-        }
+		if _, ok := fileMap[y][m]; !ok {
+			fileMap[y][m] = make(map[string][]os.FileInfo)
+		}
 
-        if _, ok := fileMap[y][m]; !ok {
-            fileMap[y][m] = make(map[string][]os.FileInfo)
-        }
+		if val, ok := fileMap[y][m][d]; ok {
+			fileMap[y][m][d] = append(val, _f)
+		} else {
+			fileMap[y][m][d] = []os.FileInfo{_f}
+		}
+	}
 
-        if val, ok := fileMap[y][m][d]; ok {
-            fileMap[y][m][d] = append(val, _f)
-        } else {
-            fileMap[y][m][d] = []os.FileInfo{_f}
-        }
-    }
+	// now create the sorted dir
+	os.Mkdir(cwd+"/"+outDir, os.FileMode(0777))
 
+	copied := 0
+	chnl := make(chan int, 1)
 
-    // now create the sorted dir
-    os.Mkdir(cwd + "/" + outDir, os.FileMode(0777))
+	for Y, monthMap := range fileMap {
+		var path = cwd + "/" + outDir + "/" + strconv.Itoa(Y)
 
-    copied := 0
-    chnl := make(chan int, 1)
+		// fmt.Println("Sorting for " + strconv.Itoa(Y));
+		os.Mkdir(path, os.FileMode(0777))
 
-    for Y, monthMap := range fileMap {
-        var path = cwd + "/" + outDir + "/" + strconv.Itoa(Y)
+		for M, dayMap := range monthMap {
+			// fmt.Println("Sorting for " + M  + " " + strconv.Itoa(Y));
+			path = cwd + "/" + outDir + "/" + strconv.Itoa(Y) + "/" + M
 
-        // fmt.Println("Sorting for " + strconv.Itoa(Y));
-        os.Mkdir(path, os.FileMode(0777))
+			os.Mkdir(path, os.FileMode(0777))
 
-        for M, dayMap := range monthMap {
-            // fmt.Println("Sorting for " + M  + " " + strconv.Itoa(Y));
-            path = cwd + "/" + outDir + "/" + strconv.Itoa(Y) + "/" + M
+			for D, files := range dayMap {
+				// fmt.Print(D);
+				path = cwd + "/" + outDir + "/" + strconv.Itoa(Y) + "/" + M + "/" + D
 
-            os.Mkdir(path, os.FileMode(0777))
+				os.Mkdir(path, os.FileMode(0777))
 
-            for D, files := range dayMap {
-                // fmt.Print(D);
-                path = cwd + "/" + outDir + "/" + strconv.Itoa(Y) + "/" + M + "/" + D
+				for _, _f := range files {
+					go func(src string, dst string, date time.Time) {
+						copyFileContents(src, dst, date)
+						defer func() { chnl <- 1 }()
+					}(*srcDir+"/"+_f.Name(), path+"/"+_f.Name(), _f.ModTime())
+				}
+			}
+		}
+	}
 
-                os.Mkdir(path, os.FileMode(0777))
+	fmt.Printf("\nCopying files\t %d/%d (%d%%)", 0, numFiles, int(0.0/numFiles*100))
 
-                for _, _f := range files {
-                    go func(src string, dst string, date time.Time) {
-                        copyFileContents(src, dst, date)
-                        defer func() {chnl <- 1}()
-                    }(*srcDir + "/" + _f.Name(), path + "/" + _f.Name(), _f.ModTime())
-                }
-            }
-        }
-    }
+	for {
+		select {
+		case <-chnl:
+			copied++
+			updateStatus(numFiles, copied)
+		default:
+		}
 
-    fmt.Printf("\nCopying files\t %d/%d (%d%%)", 0, numFiles, int(0.0/numFiles * 100))
+		if copied == numFiles {
+			break
+		}
+	}
 
-    for {
-        select {
-            case <-chnl:
-                copied++
-                updateStatus(numFiles, copied)
-            default:
-        }
-
-        if copied == numFiles {
-            break;
-        }
-    }
-    
-    fmt.Println("")
+	fmt.Println("")
 
 }
 
-
 func updateStatus(total int, num int) {
-    fmt.Printf("\r Copying files\t %d/%d (%d%%)", num, total, int(float64(num)/float64(total) * 100))
+	fmt.Printf("\r Copying files\t %d/%d (%d%%)", num, total, int(float64(num)/float64(total)*100))
 }
 
 // src http://stackoverflow.com/questions/21060945/simple-way-to-copy-a-file-in-golang
 func copyFileContents(src string, dst string, date time.Time) (err error) {
-    in, err := os.Open(src)
-    if err != nil { return err }
-    defer in.Close()
-    out, err := os.Create(dst)
-    if err != nil { return err }
-    defer out.Close()
-    _, err = io.Copy(out, in)
-    cerr := out.Close()
-    if err != nil { return err }
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, in)
+	cerr := out.Close()
+	if err != nil {
+		return err
+	}
 
-    cerr = os.Chtimes(dst, date, date)
+	cerr = os.Chtimes(dst, date, date)
 
-    return cerr
+	return cerr
 }
